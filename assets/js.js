1423939420b60789082b96c4c040b37e580120ba
Karan Vani <73671761+KaranVani@users.noreply.github.com>
2020-12-06 21:14:31 +0200

    Update js.js

--- assets/js.js
+++ assets/js.js
@@ -1,17 +1,10 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+function openNav() {
+  document.getElementById("mySidebar").style.width = "80px";
+  document.getElementById("main").style.marginLeft = "80px";
+}
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
