2687c88669942a31c2a2ad2c5690f852fc20de56
Karan Vani <73671761+KaranVani@users.noreply.github.com>
2020-12-07 16:13:01 +0200

    Update myscript.js

--- assets/myscript.js
+++ assets/myscript.js
@@ -1,3 +1,5 @@
+
+/* ---------This is for hiding/showing sidebar ------*/
 function openNav() {
   document.getElementById("mySidebar").style.width = "80px";
   document.getElementById("main").style.marginLeft = "80px";
@@ -8,3 +10,19 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+/*---------------this is for the scrolly hidey thing---------*/
+el = document.getElementById("scrolldown");
+
+var myScrollFunc = function() {
+  var y = window.scrollY;
+  if (y <= 150) {
+    el.className = "scrolldown show"
+  } else if (y<= 300) {
+	  el.className ="scrolldown mid"
+  } else {
+    el.className = "scrolldown hide"
+  }
+};
+
+window.addEventListener("scroll", myScrollFunc);
