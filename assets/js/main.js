4e7410deee127d1282f9ee8ba5ca59fd68f032c2
KaranVani <k.vani1@uni.brighton.ac.uk>
2021-06-18 17:27:49 +0200

    completely fixed drop downs and started on qualifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,15 +35,11 @@ const skillsContent = document.getElementsByClassName('skills__content'),
 
 function toggleSkills(){
     let itemClass = this.parentNode.className
-    console.log(itemClass)
-    console.log(this)
-
-    for(i =0; i < skillsContent.length;i++){
-        skillsContent[i].className = 'skills__content skills__close'
-    }
     if(itemClass === 'skills__content skills__close'){
-        this.parentNode.className = 'skills__content skills__open'
-    }
+        this.parentNode.className = itemClass.replace('close', 'open')
+        return 
+    } 
+    this.parentNode.className = itemClass.replace('open', 'close')
 }
 
 skillsHeader.forEach((el) =>{
